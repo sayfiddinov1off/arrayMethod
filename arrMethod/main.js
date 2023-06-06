24ee0acc540de8267faeb60b7680ce9de86ac5f9
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2023-06-06 06:12:24 -0700

    Add files via upload

---
+++ arrMethod/main.js
@@ -0,0 +1,25 @@
+// function push(arr, array) {
+// arr[arr.length] = array
+// return arr
+// }
+// console.log(push(["aaa"], "azamat"));
+
+// function shift(arr) {
+//     let array = [];
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         array[i] = arr[i + 1];
+//     }
+//     return array;
+// }
+// console.log(shift('cas'));
+
+
+function join(arr, arr2) {
+    let string = '';
+    for (let i = 0; i < arr.length; i++) {
+        string += arr[i];
+        string += arr2;
+    }
+    return string;
+}
+console.log(join(['My', 'name', "is", 'Azamat',], ' '));
